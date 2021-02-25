8596c654f729e07384844cdbf87bacbe755444aa
Suhas G <suhasg@Cashfrees-MacBook-Pro-3.local>
2021-02-25 12:43:30 +0530

    first commit

---
+++ Test_Framework/Manager.swift
@@ -0,0 +1,16 @@
+//
+//  Manager.swift
+//  Test_Framework
+//
+//  Created by Suhas G on 25/02/21.
+//
+
+import Foundation
+
+class Manager {
+    
+    public func returnString() -> String {
+        return "Testing"
+    }
+    
+}
