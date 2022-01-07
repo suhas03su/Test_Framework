d72962447040ba15a526c23f26d1394bb5f03311
runner <runner@Mac-1641555118223.local>
2022-01-07 11:58:48 +0000

    pushing from github actions

---
+++ Test_Framework.xcframework/ios-arm64_armv7/Test_Framework.framework/Headers/Test_Framework.h
@@ -0,0 +1,18 @@
+//
+//  Test_Framework.h
+//  Test_Framework
+//
+//  Created by Suhas G on 25/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Test_Framework.
+FOUNDATION_EXPORT double Test_FrameworkVersionNumber;
+
+//! Project version string for Test_Framework.
+FOUNDATION_EXPORT const unsigned char Test_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test_Framework/PublicHeader.h>
+
+
